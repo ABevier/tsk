832fe889612af02a7a5c3c9afbabbd959623a78a
Alan Bevier <awbevier@gmail.com>
2022-12-07 10:37:25 -0500

    context cancellation tests

---
+++ pkg/result.go
@@ -0,0 +1,14 @@
+package tasks
+
+type Result[T any] struct {
+	Val T
+	Err error
+}
+
+func NewSuccess[T any](val T) Result[T] {
+	return Result[T]{Val: val}
+}
+
+func NewFailure[T any](err error) Result[T] {
+	return Result[T]{Err: err}
+}
