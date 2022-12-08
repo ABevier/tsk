49b15bbf8c03c8abb149a2fd195d498f5cb660cf
Alan Bevier <awbevier@gmail.com>
2022-12-08 17:00:15 -0500

    clean up stopping logic

--- pkg/taskqueue.go
+++ pkg/taskqueue.go
@@ -1,8 +1,10 @@
 package tsk
 
 import (
+	"errors"
 	"log"
 	"sync"
+	"sync/atomic"
 )
 
 //TODO: rename this lol
@@ -12,8 +14,14 @@ type taskWrapper[T any, R any] struct {
 }
 
 type TaskQueue[T any, R any] struct {
+	isStopping uint32
+
 	taskChan chan taskWrapper[T, R]
+
+	// Protecting channels with N writers is hard :-(
+	waitSend *sync.WaitGroup
 	waitStop *sync.WaitGroup
+	stopOnce *sync.Once
 }
 
 type RunFunction[T any, R any] func(task T) (R, error)
@@ -46,11 +54,21 @@ func NewTaskQueue[T any, R any](maxWorkers int, run RunFunction[T, R]) *TaskQueu
 
 	return &TaskQueue[T, R]{
 		taskChan: taskChan,
+		waitSend: &sync.WaitGroup{},
 		waitStop: &waitStop,
+		stopOnce: &sync.Once{},
 	}
 }
 
 func (tq *TaskQueue[T, R]) Submit(task T) (R, error) {
+	tq.waitSend.Add(1)
+	defer tq.waitSend.Done() // Consider releasing this immediately after the write??
+
+	if atomic.LoadUint32(&tq.isStopping) == 1 {
+		//TODO: fix this error
+		return *new(R), errors.New("fix me")
+	}
+
 	resultChan := make(chan Result[R])
 	tw := taskWrapper[T, R]{task: task, resultChan: resultChan}
 
@@ -63,8 +81,11 @@ func (tq *TaskQueue[T, R]) Submit(task T) (R, error) {
 }
 
 func (tq *TaskQueue[T, R]) Stop() {
-	// TODO: add a close once sync to avoid a panic
-	close(tq.taskChan)
+	tq.stopOnce.Do(func() {
+		atomic.StoreUint32(&tq.isStopping, 1)
+		tq.waitSend.Wait()
+		close(tq.taskChan)
+	})
 
 	// TODO: allow a timeout
 	tq.waitStop.Wait()
--- pkg/taskqueue_test.go
+++ pkg/taskqueue_test.go
@@ -2,6 +2,8 @@ package tsk
 
 import (
 	"log"
+	"math/rand"
+	"sync"
 	"testing"
 	"time"
 
@@ -13,7 +15,7 @@ func TestTaskQueue(t *testing.T) {
 
 	run := func(task int) (int, error) {
 		log.Printf("i am task %d", task)
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(randWait(10, 50))
 		return task * 2, nil
 	}
 
@@ -26,4 +28,33 @@ func TestTaskQueue(t *testing.T) {
 	}
 
 	tq.Stop()
+}
+
+func TestTaskQueueCloseMultipleTimes(t *testing.T) {
+	wg := sync.WaitGroup{}
+
+	run := func(task int) (int, error) {
+		time.Sleep(randWait(10, 100))
+		return task * 2, nil
+	}
+
+	tq := NewTaskQueue(3, run)
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(n int) {
+			defer wg.Done()
+			time.Sleep(randWait(30, 100))
+			tq.Submit(n)
+			tq.Stop()
+		}(i)
+	}
+
+	wg.Wait()
+	//Test should not block or panic
+}
+
+func randWait(min, max int) time.Duration {
+	n := rand.Intn(max-min) + min
+	return time.Duration(n) * time.Millisecond
 }
